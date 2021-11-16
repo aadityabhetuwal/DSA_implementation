84e46c3b1acd13eaecbcf6eff6803e50c491260a
Aaditya Bhetuwal <aaditya3210@gmail.com>
2021-11-16 07:52:05 +0545

    Updates on code

--- heaps.cpp
+++ heaps.cpp
@@ -1,41 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <cstring>
 
 #define ll long long
 #define ull unsigned long long long
 
 using namespace std;
 
+template<typename T>
+bool cmp(T a, T b) { return a < b; }
 
+/* Heap implementation with custom comparision operator for MinHeap and MaxHeap */
 template<typename T>
 class Heap{
     T *heap_arr;
-    long long int sz;
-    long long int max_size;
-    long long int counter;
+    int sz;
+    int max_size;
+    int counter;
 
-    bool default_comp(const T &a, const T &b){
+
+    static bool default_comp(T a, T b){
         return a < b;
     }
-    bool (*_cmp_heap_elem)(T, T) = &defau;
 
-    Heap() : sz(0), counter(0) { }
-
-    Heap(size_t size) : sz(0), counter(0){
-        max_size = size;
-        heap_arr = new T[max_size];
-
-        memset(heap_arr, -1, sizeof(T) * max_size);
-    }
-
-    Heap(size_t size, bool (*cmp)(T, T) ) : sz(0), counter(0){
-        max_size = size;
-        heap_arr = new T[max_size];
-
-        memset(heap_arr, -1, sizeof(T) * max_size);
-        _cmp_heap_elem = &cmp;
-    }
+    bool (*_cmp_heap_elem)(T, T) = default_comp;
 
 
     bool empty(){ return (sz == 0); }
@@ -56,7 +46,7 @@ class Heap{
 
     void heapification_upwards(int ind){
 
-        while (ind > 0 && _comp_heap_elem( heap_arr[parent(ind)], heap_arr[ind]) ){
+        while (ind > 0 && (*_cmp_heap_elem)( heap_arr[parent(ind)], heap_arr[ind]) ){
             std::swap(heap_arr[ind], heap_arr[parent(ind)] );
             ind = parent(ind);
         }
@@ -67,40 +57,105 @@ class Heap{
         int left_ind = this->left_child(ind);
         int right_ind = this->right_child(ind);
 
-        if(left < sz && _comp_heap_elem(heap_arr[swap_ind], heap_arr[left_ind]) ){
+        if(left_ind < sz && (*_cmp_heap_elem)(heap_arr[swap_ind], heap_arr[left_ind]) ){
             swap_ind = left_ind;
         }
 
-        if(right < sz && _comp_heap_elem(heap_arr[swap_ind], heap_arr[right_ind]) ){
+        if(right_ind < sz && (*_cmp_heap_elem)(heap_arr[swap_ind], heap_arr[right_ind]) ){
             swap_ind = right_ind;
         }
 
         if(swap_ind != ind){
-            std::swap(heap[swap_ind], heap[ind]);
+            std::swap(heap_arr[swap_ind], heap_arr[ind]);
             heapification_downwards(swap_ind);
         }
     }
 
+    
 public :
+
+    Heap() : sz(0), counter(0) { }
+
+    Heap(int size) : sz(0), counter(0){
+        max_size = size;
+        heap_arr = new T[max_size];
+
+        memset(heap_arr, -1, sizeof(T) * max_size);
+    }
+
+    Heap(int size, bool (*cmp)(T, T) ) : sz(0), counter(0){
+        max_size = size;
+        heap_arr = new T[max_size];
+
+        memset(heap_arr, -1, sizeof(T) * max_size);
+
+        _cmp_heap_elem = cmp;
+    }
+
+    ~Heap(){
+        delete heap_arr;
+    }
+
     void push(T data){
         
-        if(!this->full()){
-            std::cerr << "Heap is full so data cannot be inserted.\n";
+        if(this->full()){
+            std::cerr << "Error : Cannot insert element into a full heap.\n";
             return;
         }
 
         sz++;
-        heap_arr[counter++] = data;
+        heap_arr[counter] = data;
 
         heapification_upwards(counter);
+        counter++;
     }
 
-    vo
+    T top(){
+        return heap_arr[0];
+    }
+
+    void pop_index(int ind){
+
+        if(this->empty()){
+            std::cerr << "Error: Cannot remove element from empty heap.\n";
+            return;
+        }
+
+        this->heap_arr[ind] = this->heap_arr[counter];
+        this->sz--;
+        this->counter--;
+
+        this->heapification_downwards(ind);
+    }
+
+    void pop_root(){
+        int i = 0;
+        this->pop_index(0);
+    }
+
+    void display(){
+        std::cout << "Heap elements in order are :\n[";
+
+        for(int i = 0; i < sz - 1; i++){
+            std::cout << heap_arr[i] << ", ";
+        }
+        std::cout << heap_arr[sz - 1] << "]\n";
+    }
 };
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+
+    Heap<int> he(9, cmp);
+
+    he.push(45);
+    he.push(40);
+
+    he.display();
+
+    he.push(75);
+
+    he.display();
+
 
     return 0;
 }
